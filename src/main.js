88523b358747e72a69cc1849e91fe31c941694db
Nguyễn Như Đạt <nguyennhudatfreefire@gmail.com>
2026-09-21 15:18:28 +0700

    CN

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ document.querySelector('#app').innerHTML = `
   </div>
   <div>
     <h1>JS nâng cao</h1>
+    <h3>ĐẠT ĐZ FREE FIRE</h3>
     <p>Edit <code>src/main.js</code> and save to test <code>HMR</code></p>
   </div>
   <button id="counter" type="button" class="counter"></button>
